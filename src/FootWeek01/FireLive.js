7073def0a9a2cb986f57993021c3f26b42c9fadb
NoteCom <ahwjs1022@gmail.com>
2023-02-26 22:40:14 +0900

    realtime firebase , get write onLoad

--- src/FootWeek01/FireLive.js
+++ src/FootWeek01/FireLive.js
@@ -1,16 +1,49 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
+import { initializeApp } from "firebase/app";
+import firebaseConfig from './firebase.js';
+import { getDatabase, ref, onValue,set, add, push} from "firebase/database";
+
 function FireLive(props){
+
+    const [UserName,setUserName] = useState("guest")
+    const [Message,setMessage]=useState("ahwjs1022")
+    const [Email,setEmail]=useState("")
     
+    useEffect(()=>{
+        const db = getDatabase();
+        const starCountRef = ref(db, 'chatting1/' + "guest" );
+        onValue(starCountRef, (snapshot) => {
+        const data = snapshot.val();
+        console.log(data)
+        }); 
+    },[])
 
     const location = useLocation();
-    console.log(location)
     const { key1 } = useParams();
-    console.log(key1)
+
+    function writeUserData(userId, name, email, imageUrl) {
+        const timeCheck = new Date();
+        console.log(timeCheck)
+    const db = getDatabase();
+    push(ref(db, 'chatting1/' + "guest"), { //쿠기계정
+      username: UserName,                      //
+      email   : Email,                                         
+      message : Message,
+      asd :String(timeCheck),
+      
+    });
+  }
+
     return(
         <div>
-            {key1}
+            {/* {location.state.name}
+            {key1} */}
+            {Message}
+           <br></br>
+           <input type="text" onChange={(e)=>{setMessage(e.target.value)}}></input> 
+           <button onClick={()=>writeUserData()}></button>
         </div>
     )
 }export default FireLive
