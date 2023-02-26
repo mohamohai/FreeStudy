7cd579e758ba22fd9a86f2ad1b04f325ff44fad9
NoteCom <ahwjs1022@gmail.com>
2023-02-26 15:00:14 +0900

    push test checkout -b

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      여기서 테스트를 해봅시다.ddddd
+    <div style={{backgroundColor:"red"}} className="App">
+      firebase 연동
+      <div></div>
     </div>
   );
 }
