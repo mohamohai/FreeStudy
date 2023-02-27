8b0cfcab2943d22c7739adc8091575bad1f8c7b3
jh <ahwjs1022@gmail.com>
2023-02-27 13:38:48 +0900

    test in

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import './App.css';
 import PageView from './live/PageView';
 import { BrowserRouter, Router, Route, Switch, Link,Routes } from 'react-router-dom';
 
-import PageView from './live/PageView';
+
 function App() {
   return (
     <div className="App">
 
       <BrowserRouter>
 				<Routes>
-          <Route path='/' element={<FreeStudy />}></Route>
+          <Route path='/' element={<PageView />}></Route>
 
           <Route path="/mainpage" element={<PageView />}></Route>
 					<Route path="/firelive" element={<PageView />}></Route>
           <Route path='/firelive/:key1/:key2' element={<PageView />}></Route>
 
-					<Route path="*" element={<FreeStudy />}></Route>
+					<Route path="*" element={<PageView />}></Route>
           
 				</Routes>
 			</BrowserRouter>
